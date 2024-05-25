ab7369f1d2b513af138e2fa2288efdc67bdb8238
Tyuiu.MalsagovUA@outlook.com <mals.umar@yandex.ru>
2024-05-26 00:34:35 +0500

    Add project files.

---
+++ Tyuiu.UsoltsevAD.Sprint2.Task0.V28.Lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Tyuiu.UsoltsevAD.Sprint2.Task0.V28.Lib
+{
+    public class DataService : ISprint2Task0V28
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            bool[] res = new bool[6];
+            res[0] = x == y;
+            res[1] = x + 624 != y;
+            res[2] = y < x;
+            res[3] = x * 2 > y;
+            res[4] = x + 630 <= y;
+            res[5] = x - 1000 >= y;
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.UsoltsevAD.Sprint2.Task0.V28.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.UsoltsevAD.Sprint2.Task0.V28.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.UsoltsevAD.Sprint2.Task0.V28.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("72693430-f19c-4121-9fd8-9398908e93a9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
